3f4cd6e2bfc0a3daa0df417732257165e0428f96
Shubham Jain <shubhamjain0594@gmail.com>
2018-04-23 22:20:34 +0100

    Modifying subproc for json handling.

--- codejail/subproc.py
+++ codejail/subproc.py
@@ -53,7 +53,7 @@ def run_subprocess(
     if stdin:
         stdin = stdin.encode()
     stdout, stderr = subproc.communicate(stdin)
-    return subproc.returncode, str(stdout, encoding='ascii'), stderr
+    return subproc.returncode, stdout.encode('ascii'), stderr
 
 
 def set_process_limits(rlimits):       # pragma: no cover
