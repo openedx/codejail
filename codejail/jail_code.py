e1bbf5a6ecfbdf22628f4b41482d9106e5b5bbaa
Stuart Young <syoung@edx.org>
2019-07-26 15:02:21 -0400

    fix file opening

--- codejail/jail_code.py
+++ codejail/jail_code.py
@@ -205,15 +205,15 @@ def jail_code(command, code=None, files=None, extra_files=None, argv=None,
 
         # Create the main file.
         if code:
-            with io.open(os.path.join(homedir, "jailed_code"), "w") as jailed:
-                jailed.write(code)
+            with io.open(os.path.join(homedir, "jailed_code"), "wb") as jailed:
+                jailed.write(io.BytesIO().write(code))
 
             argv = ["jailed_code"] + argv
 
         # Create extra files requested by the caller:
         for name, content in extra_files or ():
-            with io.open(os.path.join(homedir, name), "w") as extra:
-                extra.write(content)
+            with io.open(os.path.join(homedir, name), "wb") as extra:
+                extra.write(io.BytesIO().write(content))
 
         cmd = []
         rm_cmd = []
