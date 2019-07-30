fe893a34bc756075164bc489ec3a18ef35fa9b8b
Stuart Young <syoung@edx.org>
2019-07-30 14:19:50 -0400

    little fix

--- codejail/tests/test_jail_code.py
+++ codejail/tests/test_jail_code.py
@@ -14,6 +14,7 @@ import time
 import unittest
 
 import mock
+from builtins import bytes
 from nose.plugins.skip import SkipTest
 
 from codejail.jail_code import jail_code, is_configured, set_limit, LIMITS
