c7cef318329c146778092e45a016fed67c5cb906
Feanil Patel <feanil@edx.org>
2019-09-26 11:39:44 -0400

    Bump the version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="codejail",
-    version="2.0",
+    version="2.1",
     packages=['codejail'],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
